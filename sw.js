f56a1dd590cf339f3916e9c89bcdb721a1ff217e
aljabali2001 <algablihassan@gmail.com>
2025-09-28 12:19:47 +0300

    Add minimal service worker file sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
+const CACHE_NAME = 'invoice-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open('invoice-app-v1').then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request).then(networkResponse => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
